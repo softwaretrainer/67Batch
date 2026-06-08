e1680416b4c7cca62a792ebe61d295c9baeb82fb
softwaretrainer <softwaretrainer.suresh@gmail.com>
2026-06-08 13:49:00 +0530

    added console statements.

--- tests/TC001.spec.ts
+++ tests/TC001.spec.ts
@@ -7,4 +7,5 @@ test('TC001_Login_Logout', async ({ page }) => {
     await obj.openApplication();
     await obj.login();
     await obj.logout();
+    console.log('TC001_Login_Logout executed successfully');
 });
